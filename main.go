f6c756c5b6ebd7bf8c4496abf3b090fd041b7bb5
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-03-26 20:58:10 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+type GOOD struct {
+	id int64
+	name string 
+	price int 
+	additional [] string
+}
+
 func main(){
 	//variables
 	var type_of_drink string 
@@ -13,6 +20,13 @@ func main(){
 	
 	//condition 
 	if type_of_drink == "coffe"{
+		coffe := GOOD{
+			id :1,
+			name : "coffe",
+			price: 45,
+			additional: []string{"nothing","got"},
+		}
+		fmt.Println(coffe.price)
 		fmt.Printf("Thanks for %s",type_of_drink)
 	}
 	
